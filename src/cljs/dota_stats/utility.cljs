351f37eea1bd6f32fa3859bc989d95d3dfacafaa
Olle Linderos <olle@linderos.nu>
2019-01-27 03:02:27 +0100

    to-percentage and calc tests

--- src/cljs/dota_stats/utility.cljs
+++ src/cljs/dota_stats/utility.cljs
@@ -14,12 +14,18 @@
 (defn merge-results [wins losses]
   (sort-by :match_id (concat wins losses)))
 
+(defn to-percentage [wins total]
+  (* (/ wins total) 100))
+
 (defn calc-winrate [matches]
   (loop [wins 0
          i 1]
     (if (= i (count matches))
-      (* (/ wins i) 100)
+      (prn "hey")
       (recur
-       (if (eq (:result match) 0)
-         (prn (* (/ wins i) 100)))
-       (inc i)))))
+       (do
+         (inc wins)
+         (conj (to-percentage wins i) data)
+       (inc i))))))
+
+(calc-winrate [{:result 0} {:result 1} {:result 0} {:result 1} {:result 1}])
