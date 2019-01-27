0e38787ce2e7a772dd7b78dde374c0df64dd74b3
Olle Linderos <olle@linderos.nu>
2019-01-27 02:23:30 +0100

    Calc winrate function

--- src/cljs/dota_stats/utility.cljs
+++ src/cljs/dota_stats/utility.cljs
@@ -1,4 +1,6 @@
-(ns dota-stats.utility)
+(ns dota-stats.utility
+  (:require [dota-stats.state :as state]
+            [reagent.core :as r]))
 
 (defn how-long-ago? [date]
   (Math/floor
@@ -9,3 +11,15 @@
 (defn sort-by-recency [vec]
   (reverse (sort-by :last_match_time vec)))
 
+(defn merge-results [wins losses]
+  (sort-by :match_id (concat wins losses)))
+
+(defn calc-winrate [matches]
+  (loop [wins 0
+         i 1]
+    (if (= i (count matches))
+      (* (/ wins i) 100)
+      (recur
+       (if (eq (:result match) 0)
+         (prn (* (/ wins i) 100)))
+       (inc i)))))
